19a175ef8a0d5d53a60618282336463edfd3666a
John Savill <john@savilltech.com>
2026-08-16 11:16:33 -0700

    Setup for AI-103 modules

---
+++ AI/AIBasicPyReq.ps1
@@ -0,0 +1,4 @@
+python -m pip install azure-ai-projects
+python -m pip install agent-framework-foundry agent-framework-orchestrations
+python -m pip install azure-ai-textanalytics
+python -m pip install azure-ai-translation-text
